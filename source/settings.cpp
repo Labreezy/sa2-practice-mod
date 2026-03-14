7c4a2e7005afc9a55cbd6c14ad365e729a0894ca
Katie4A <37027757+Katie4A@users.noreply.github.com>
2026-03-14 04:10:15 -0700

    fixed a couple lines LOL

--- source/settings.cpp
+++ source/settings.cpp
@@ -15,13 +15,14 @@ void Settings::setESG() {
 
 void Settings::RenderTab() {
 	if (ImGui::CollapsingHeader("Settings")) {
-		ImGui::Checkbox("Emblem Skip Glitch", &esgToggle);
+		ImGui::Checkbox("Emblem Skip Glitch toggle", &esgToggle);
+		ImGui::SetItemTooltip("If checked, no emblem cutscenes will be played. Note that you need certain emblems to unlock certain modes \n(i.e., to get last story you need both the Hero and Dark credits emblems.)");
 		ImGui::Checkbox("PP Skip Reset", &ppSkipToggle);
 		ImGui::SetItemTooltip("If checked, reset the variable that pushes the kart forward for 120 frames on every load/restart. Only relevant for Route 101/280.");
 		ImGui::Checkbox("Lock Egg Golem Death/Restart Counter", &egRestartLockToggle);
 		ImGui::SetItemTooltip(
-			"Adjusts the number of times the player has died or restarted."
-			"This is relevant for Egg Golem, where the boss delays itself"
+			"Adjusts the number of times the player has died or restarted.\n"
+			"This is relevant for Egg Golem, where the boss delays itself \n"
 			"depending on how many times you have died/restarted."
 		);
 		ImGui::SameLine();
